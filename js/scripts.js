2df8a1f1c98fd87fb794f6d971d6803096dbd64c
Lawrence <lawrenslava1@gmail.com>
2018-03-28 14:14:44 +0300

    Add javascript functions and separate business and ui logics

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+function pingpong(startPoint) {
+  if ((startPoint%3===0 && startPoint%5===0)) {
+    return('pingpong');
+  }
+
+  else if (startPoint%5===0) {
+    return('pong');
+  }
+  else if (startPoint % 3===0) {
+    return ('ping');
+  }
+  else {
+    return startPoint;
+  }
+}
+
 $(document).ready(function() {
   $('#submit').click(function(event) {
-    var num = parseInt($('input#number').val());
+    var userNumber = parseInt($('input#number').val());
     event.preventDefault();
-    var output=pingpong(num);
+    $('ul.numbersList').empty();
+    for (var startPoint=1; startPoint <= userNumber; startPoint +=1 ) {
+      $('ul.numbersList').append('<li>' + pingpong(startPoint) + '</li>');
+    }
   });
-  function pingpong(num){
-    
-  }
 });
